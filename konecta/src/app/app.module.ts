d634a8811410bf34e62f7ca04e25268bdfc6a88a
vivisgalvis <vivigalvis.89@gmail.com>
2021-02-08 15:30:23 -0500

    creacion de componentes

--- konecta/src/app/app.module.ts
+++ konecta/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './Components/layout/layout.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { CardsContainerComponent } from './Components/cards-container/cards-container.component';
+import { CardsComponent } from './Components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    NavComponent,
+    CardsContainerComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
