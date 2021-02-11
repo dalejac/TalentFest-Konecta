3ecfb1acedb47d12a4559b4123e8330648af68c0
Oriana <oriana.nohemi22@gmail.com>
2021-02-11 09:56:38 -0500

    ruta al componente de detalle

--- konecta/src/app/Components/cards-container/cards-container.component.ts
+++ konecta/src/app/Components/cards-container/cards-container.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/Services/data.service';
-import Quill from 'quill';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -10,8 +9,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./cards-container.component.sass']
 })
 export class CardsContainerComponent implements OnInit {
-
-  @Input() Article: any;
+  @Input() element: any;
 
   data$: Observable<any[]>
   constructor(private dataService: DataService) { }
@@ -20,7 +18,6 @@ export class CardsContainerComponent implements OnInit {
     this.data$ = this.dataService.data$.pipe(
       map((res) => {
         let image = '';
-        console.log(res)
         for (let i= 0; i < res.length; i++){
           const curr = JSON.parse(res[i]["obj"] || '{}').ops || [];
           for (let m= 0; m < curr.length; m++){
@@ -36,26 +33,5 @@ export class CardsContainerComponent implements OnInit {
 
   }
 
-  show(element) {
-    let arrData = element.obj;
-    console.log(element)
-    const fix_article = JSON.parse(element.obj).ops;
-    for (let i = 0; i < fix_article.length; i++) {
-      if (fix_article[i]["insert"].image) {
-        fix_article[i]["insert"].image = "https://nik.grupokonecta.co:7070/" + fix_article[i]["insert"].image
-      }
-    }
-
-    const quill = new Quill('#contenedor', {
-      theme: 'bubble'
-    });
-    
-    try{
-      quill.setContents([{ insert: '\n' }]);
-      quill.updateContents(fix_article);
-    }
-    catch(err){
-    }
-  }
 }
 
