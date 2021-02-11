4f3587fc6a6bd5d0fa9a411ee4e1f3a5edd93701
Oriana <oriana.nohemi22@gmail.com>
2021-02-11 11:27:33 -0500

    detalle del articulo al hacer click

--- konecta/src/app/Components/card-details/card-details.component.ts
+++ konecta/src/app/Components/card-details/card-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { element } from 'protractor';
 import Quill from 'quill';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 import { elementAt, map } from 'rxjs/operators';
 import { DataService } from 'src/app/Services/data.service';
 
@@ -11,43 +12,42 @@ import { DataService } from 'src/app/Services/data.service';
   styleUrls: ['./card-details.component.sass']
 })
 export class CardDetailsComponent implements OnInit {
+  @ViewChild('container') container: ElementRef;
   id: any
   data$
 
   constructor(private dataService: DataService, private _route: ActivatedRoute) {
     this.id = this._route.snapshot.paramMap.get('id')
-    console.log(this.id)
-    // this.show(this.id)
+    this.showDetail(this.id)
    }
 
   ngOnInit(): void {
   }
   
-  // show(id) {
-  //   this.data$ = this.dataService.getDataId(id)
-  //   console.log(this.data$)
-  //   .subscribe((data: any) => {
-  //     map((element) => {
-  //       console.log(element)
-  //       let arrData = element.obj;
-  //       const fix_article = JSON.parse(arrData).ops;
-  //       for (let i = 0; i < fix_article.length; i++) {
-  //         if (fix_article[i]["insert"].image) {
-  //           fix_article[i]["insert"].image = "https://nik.grupokonecta.co:7070/" + fix_article[i]["insert"].image
-  //         }
-  //       }
-  //     })
-
-  //   const quill = new Quill('#contenedor', {
-  //     theme: 'bubble'
-  //   });
-    
-  //   try{
-  //     quill.setContents([{ insert: '\n' }]);
-  //     quill.updateContents(fix_article);
-  //   }
-  //   catch(err){
-  //   }
-  // }
-
+  showDetail(id) {
+    this.data$ = this.dataService.getDataId(id)
+    .pipe(
+      map((element) => {
+        const quill = new Quill(this.container.nativeElement, {
+          theme: 'bubble'
+        });
+        let arrData = element['obj'];
+        const fix_article = JSON.parse(arrData).ops;
+        for (let i = 0; i < fix_article.length; i++) {
+          if (fix_article[i]["insert"].image) {
+            fix_article[i]["insert"].image = "https://nik.grupokonecta.co:7070/" + fix_article[i]["insert"].image
+          }
+        }
+        try{
+          quill.setContents([{ insert: '\n' }]);
+          quill.updateContents(fix_article);
+        }
+        catch(err){
+        }
+      })
+    )
+    .subscribe((data: any) => {
+      return data
+    })
+  }
 }
